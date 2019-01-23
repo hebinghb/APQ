09abef78758eba2b82c48b8fa5c8b41cf0230bc0
hebinghb <45085930+hebinghb@users.noreply.github.com>
2019-01-23 12:43:41 -0500

    New software support
    Add support to output format of skyline

--- R/APQ.R
+++ R/APQ.R
@@ -4,55 +4,114 @@ APQ.default <- function(ms_data, datatype, ...) {
 		   stop("Please select a valid datatype. e.g.:  \"DIA\", \"DDA\"")
 	   }
 	   if(datatype == "DIA"){
-     total_ms2 <- as.list(tapply(ms_data$F.PeakArea,ms_data$R.FileName,sum))
-     samples <- unique(ms_data$R.FileName)
-     final_APQ <- list();
-     for(sample_name in samples){
-        sample_data <- subset(ms_data, ms_data$R.FileName==sample_name) 
-        protein_ms2 <- as.matrix(tapply(sample_data$F.PeakArea,sample_data$PG.ProteinAccessions,sum))
-        new_ms2 <- Redistribution(protein_ms2)
-        protein_APQ <- 1000*new_ms2/as.numeric(total_ms2[sample_name])
-        final_APQ <- cbind(final_APQ,protein_APQ)
-     }
-     colnames(final_APQ) <- samples
-     return(final_APQ)
+	       total_ms2 <- as.list(tapply(ms_data$PeakArea,ms_data$SampleName,sum))
+         samples <- unique(ms_data$SampleName)
+         final_APQ <- list()
+         for(sample_name in samples){
+            sample_data <- subset(ms_data, ms_data$SampleName==sample_name) 
+            protein_ms2 <- as.matrix(tapply(sample_data$PeakArea,sample_data$ProteinName,sum))
+            new_ms2 <- Redistribution(protein_ms2)
+            protein_APQ <- 1000*new_ms2/as.numeric(total_ms2[sample_name])
+            final_APQ <- cbind(final_APQ,protein_APQ)
+         }
+         colnames(final_APQ) <- samples
+         final_APQ[final_APQ == 0] <- NA
+         final_APQ <- final_APQ[apply(final_APQ, 1, function(x) !all(is.na(x))),]
+         return(final_APQ)
      }
      if(datatype == "DDA"){
-     total_ms1 <- apply(ms_data[-(1:3)],2,sum)
-     samples <- colnames(ms_data[-(1:3)])
-     final_APQ <- list();
-     for(sample_name in samples){
-        protein_ms1 <- na.omit(as.matrix(tapply(ms_data[[sample_name]],ms_data$Proteins,sum)))
-        new_ms1 <- Redistribution(protein_ms1)
-        protein_APQ <- 1000*new_ms1/as.numeric(total_ms1[sample_name])
-        final_APQ <- cbind(final_APQ,protein_APQ)
-     }
-     colname <- gsub("Intensity.","",samples)
-     colnames(final_APQ) <- colname
-     
-     return(final_APQ)
+       total_ms1 <- apply(ms_data[-(1:3)],2,sum)
+       samples <- colnames(ms_data[-(1:3)])
+       final_APQ <- list()
+       for(sample_name in samples){
+          protein_ms1 <- as.matrix(tapply(ms_data[[sample_name]],ms_data$Proteins,sum))
+          new_ms1 <- Redistribution(protein_ms1)
+          protein_APQ <- 1000*new_ms1/as.numeric(total_ms1[sample_name])
+          final_APQ <- cbind(final_APQ,protein_APQ)
+       }
+       colname <- gsub("Intensity.","",samples)
+       colnames(final_APQ) <- colname
+       final_APQ[final_APQ == 0] <- NA
+       final_APQ <- final_APQ[apply(final_APQ, 1, function(x) !all(is.na(x))),]   
+       final_APQ <- Clean(final_APQ)
+       final_APQ[final_APQ == 0] <- NA
+       final_APQ <- final_APQ[apply(final_APQ, 1, function(x) !all(is.na(x))),]       
+       return(final_APQ)
      }
 }
 
 Redistribution <- function(protein_ms){
       proteins <- rownames(protein_ms)
       share2uniq_ms <- protein_ms
+      proteinfrequency <- vector()
       for(protein in proteins[grep(";", proteins)]){ 
           total_uniq_ms <- 0 
           for(temp_protein in unlist(strsplit(protein,";"))){
-             if(temp_protein %in% proteins){ 
+             if(temp_protein %in% proteins && !is.na(protein_ms[temp_protein,1])){ 
                total_uniq_ms <- total_uniq_ms + protein_ms[temp_protein,1]
              }
+             
+
           }
           
           if(total_uniq_ms > 0){
              for(temp_protein in unlist(strsplit(protein,";"))){
-                if(temp_protein %in% proteins){ 
+                if(temp_protein %in% proteins && !is.na(protein_ms[temp_protein,1])){ 
                   share2uniq_ms[temp_protein,1] <- share2uniq_ms[temp_protein,1] + (protein_ms[temp_protein,1]/total_uniq_ms)*protein_ms[protein,1]
                   }
              }
-             share2uniq_ms[protein,1] <- 0
+             share2uniq_ms[protein,] <- NA
           }
        }
       return(share2uniq_ms)
+} 
+
+Clean <- function(APQ){
+      proteins <- rownames(APQ)
+      clean_apq <- APQ
+      clean_apq[is.na(clean_apq)] <- 0
+      #Count frequency of protein in protein groups
+      proteinfrequency <- vector()
+      for(protein in proteins[grep(";", proteins)]){
+          for(temp_protein in unlist(strsplit(protein,";"))){
+             if(is.na(proteinfrequency[temp_protein])){
+                if(temp_protein %in% proteins){
+                  proteinfrequency[temp_protein] <- 10
+                }else{
+                  proteinfrequency[temp_protein] <- 1
+                }
+             }else{
+                proteinfrequency[temp_protein] <- proteinfrequency[temp_protein]+1
+             } 
+           }        
+      }
+      #Clean low confidence protein groups
+      for(protein in proteins[grep(";", proteins)]){
+          temp_proteins <- unlist(strsplit(protein,";"))
+          if(max(proteinfrequency[temp_proteins]) != min(proteinfrequency[temp_proteins])){
+             max_proteins <- c()
+             for(temp_protein in temp_proteins){
+                 if(proteinfrequency[temp_protein] == max(proteinfrequency[temp_proteins])){
+                    max_proteins <- c(max_proteins,temp_protein)
+                 }
+             }
+             new_protein <- paste0(sort(max_proteins),collapse=";")
+             temp_rowname <- rownames(clean_apq)
+             if(!new_protein %in% temp_rowname){
+               temp_rowname[which(temp_rowname == protein)] <- new_protein
+               rownames(clean_apq) <- temp_rowname
+             }else{
+             clean_apq[new_protein,] <- unlist(clean_apq[new_protein,]) + unlist(clean_apq[protein,]) 
+             clean_apq[protein,] <- NA
+             }
+          }else{
+               new_protein <- paste0(sort(temp_proteins),collapse=";")
+               temp_rowname <- rownames(clean_apq)
+               if(!new_protein %in% temp_rowname){  
+                 temp_rowname[which(temp_rowname == protein)] <- new_protein
+                 rownames(clean_apq) <- temp_rowname
+               }
+          }
+      } 
+      return(clean_apq)
 }
--- R/Import.R
+++ R/Import.R
@@ -1,37 +1,113 @@
 Import<-function(filename, filetype, ...) UseMethod("Import")
-Import.default <- function(filename, ...) {
-	if (!grepl("\\.csv$",filename) && !grepl("\\.tsv$",filename) ) {
-		stop("Please use a valid output filetype from Spectronaut. e.g.:  \"csv\", \"tsv\"")
+
+Import.default <- function(filename, filetype, ...) {
+	if (!filetype %in% c("spectronaut","skyline")) {
+		stop("Please select a valid filetype. Options:  \"spectronaut\", \"skyline\"")
 	}
-  if (grepl("\\.csv$",filename)) {
+	if (!grepl("\\.csv$",filename) && !grepl("\\.tsv$",filename) ) {
+		stop("Please use a valid file format. e.g.:  \"csv\", \"tsv\"")
+	}
+	if(filetype == "spectronaut"){ 
+	  cat("Filetype: spectronaut\n")
+    if (grepl("\\.csv$",filename)) {
+    cat("File format: csv\n")
 		# Spectronaut csv
+		cat("Filename: ",filename,"\n")
+		cat("Loading data...\n")
 		raw_data <- read.csv(file=filename)
-		ms_data<-raw_data[c("R.FileName","PG.ProteinAccessions","EG.StrippedSequence","F.PeakArea")]
+		cat("Done\n")
+		cat("Transforming data...\n")	
+		ms_data <- raw_data[c("R.FileName","PG.ProteinAccessions","EG.StrippedSequence","F.PeakArea")]
+		ms_data <- subset(ms_data,ms_data$F.PeakArea != "NaN")
+		colnames(ms_data) <- c("SampleName","ProteinName","PeptideSequence","PeakArea")
+		cat("Done\n")
 		return(ms_data)
-  }
-  if (grepl("\\.tsv$",filename)) {
+   }
+    if (grepl("\\.tsv$",filename)) {
+    cat("File format: tsv\n")
 		# Spectronaut tsv
+		cat("Filename: ",filename,"\n")
+		cat("Loading data...")
 		raw_data <- read.csv(file=filename,sep = "\t")
-		ms_data<-raw_data[c("R.FileName","PG.ProteinAccessions","EG.StrippedSequence","F.PeakArea")]
+		cat("Done\n")
+		cat("Transforming data...\n")			
+		ms_data <- raw_data[c("R.FileName","PG.ProteinAccessions","EG.StrippedSequence","F.PeakArea")]
+		ms_data <- subset(ms_data,ms_data$F.PeakArea != "NaN")
+		colnames(ms_data) <- c("SampleName","ProteinName","PeptideSequence","PeakArea")			 
+		cat("Done\n")
 		return(ms_data)
-  } 
-  if(grepl("peptides\\.txt$",filename)){
-  
-  } 
+   } 
+  } else if(filetype == "skyline") {
+    cat("Filetype: skyline\n")
+    if (grepl("\\.csv$",filename)) {
+    cat("File format: csv\n")
+		# Skyline csv
+		cat("Filename: ",filename,"\n")
+		cat("Loading data...\n")
+		raw_data <- read.csv(file=filename)
+		cat("Done\n")
+		cat("Transforming data...\n")			
+		ms_data <- raw_data[c("Replicate.Name","Protein.Name","Peptide.Sequence","Fragment.Ion","Area")]
+		ms_data <- subset(ms_data,ms_data$Area != "#N/A")
+		ms_data <- Reformat(ms_data)
+		ms_data <- ms_data[c("Replicate.Name","Protein.Name","Peptide.Sequence","Area")]
+		colnames(ms_data) <- c("SampleName","ProteinName","PeptideSequence","PeakArea")
+		ms_data$PeakArea <- as.numeric(as.character(ms_data$PeakArea))
+		cat("Done\n")
+		return(ms_data)
+    }
+    if (grepl("\\.tsv$",filename)) {
+    cat("File format: tsv\n")
+		# Skyline tsv
+		cat("Filename: ",filename,"\n")
+		cat("Loading data...\n")		
+		raw_data <- read.csv(file=filename,sep = "\t")
+		cat("Done\n")
+		cat("Transforming data...\n")			
+		ms_data <- raw_data[c("Replicate.Name","Protein.Name","Peptide.Sequence","Fragment.Ion","Area")]
+		ms_data <- subset(ms_data,ms_data$Area != "#N/A")
+		ms_data <- Reformat(ms_data)
+		ms_data <- ms_data[c("Replicate.Name","Protein.Name","Peptide.Sequence","Area")]
+		colnames(ms_data) <- c("SampleName","ProteinName","PeptideSequence","PeakArea")
+		ms_data$PeakArea <- as.numeric(as.character(ms_data$PeakArea))
+		cat("Done\n")
+		return(ms_data)
+    }      
+  }
 }
+
 Import.DDA <- function(filename, ...) {
 	if (!grepl("peptides\\.txt$",filename) ) {
 		stop("Please use peptides.txt from MaxQuant")
 	}
 		# MaxQuant peptides
+		cat("Filetype: maxquant\n")
+		cat("Filename: ",filename,"\n")
+		cat("Loading data...\n")
 		raw_data <- read.csv(file=filename,sep = "\t")
+		cat("Done\n")
+		cat("Transforming data...\n")	
 		colname <- colnames(raw_data)
 		# Remove Contamination
 		Proteins <- raw_data$Proteins
 		temp_data <- raw_data[-(grep("CON_",Proteins)),]
-		Proteins <- temp_data$Leading
+		Proteins <- temp_data$Leading.razor.protein
 		clean_data <- temp_data[-(grep("REV_",Proteins)),]
-		ms_data<-clean_data[c("Proteins","Gene.names","Protein.names",colname[grep("Intensity.",colname)])]
+		ms_data <- clean_data[c("Proteins","Gene.names","Protein.names",colname[grep("Intensity.",colname)])]
+		temp_data <- clean_data[c(colname[grep("Intensity.",colname)])]
+		ms_data <- ms_data[apply(temp_data, 1, function(x) !all(x == 0)),]
+		cat("Done\n")
 		return(ms_data)
   
+}
+
+Reformat <- function(ms_data){
+   temp_data1 <- data.frame(ID=paste0(ms_data$Replicate.Name,ms_data$Peptide.Sequence,ms_data$Area,ms_data$Fragment.Ion),Protein.Name=ms_data$Protein.Name)
+   temp_data2 <- unique(data.frame(ID=paste0(ms_data$Replicate.Name,ms_data$Peptide.Sequence,ms_data$Area,ms_data$Fragment.Ion),Replicate.Name=ms_data$Replicate.Name,Peptide.Sequence=ms_data$Peptide.Sequence,Area=ms_data$Area))
+   temp_data1 <- as.data.frame.table(by(data=temp_data1,INDICES = temp_data1$ID,FUN = function(x){paste0(x$Protein.Name,collapse = ';')}))
+   colnames(temp_data1) <- c("ID","Protein.Name")
+   temp_data <- merge(temp_data1,temp_data2)
+   temp_data <- temp_data[c("Replicate.Name","Protein.Name","Peptide.Sequence","Area")]
+   return(temp_data)
+   
 }
