a5d5b567d4fa312e74968dbfe4579a02633c81ad
hebinghb <45085930+hebinghb@users.noreply.github.com>
2020-03-04 10:53:57 -0500

    Update Import.R

--- R/Import.R
+++ R/Import.R
@@ -101,7 +101,7 @@ Import.DDA <- function(filename, ...) {
 		}else{
 		   clean_data <- temp_data
 		}
-		ms_data <- clean_data[c("Proteins",colname[grep("Intensity.",colname)])]
+		ms_data <- clean_data[c("Proteins",colname[grep("Intensity",colname)])]
 		temp_data <- clean_data[c(colname[grep("Intensity",colname)])]
 		ms_data <- ms_data[apply(temp_data, 1, function(x) !all(x == 0)),]
 		cat("Done\n")
